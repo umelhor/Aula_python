1c4fa930826ae78c15e9ab436867fcee4698d19b
Paulo Ricardo <102255702+umelhor@users.noreply.github.com>
2024-09-16 19:32:21 -0300

    Delete att_2.py

--- att_2.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-num3 = float(input("Digite o terceiro número: "))
-
-maior = max(num1, num2, num3)
-print(f"O maior número é: {maior}")
