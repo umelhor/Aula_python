0b3fa31a47b1c819b96ada2823d5dc4943b2cede
Paulo Ricardo <102255702+umelhor@users.noreply.github.com>
2024-09-16 19:37:31 -0300

    Delete Lab_prog/ExercicioA.py

--- Lab_prog/ExercicioA.py
+++
@@ -1,6 +0,0 @@
-cateto_b = float(input("Digite o valor do cateto B: "))
-cateto_c = float(input("Digite o valor do cateto C: "))
-
-hipotenusa = (cateto_b ** 2 + cateto_c ** 2) ** 0.5
-
-print(f"A hipotenusa é: {hipotenusa:.2f}")
