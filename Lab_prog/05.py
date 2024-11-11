0669497c4e5dabc8a458ff852b61125dfa2294e7
Paulo Ricardo <102255702+umelhor@users.noreply.github.com>
2024-11-11 20:38:07 -0300

    Atualizar o Lab_prog

---
+++ Lab_prog/05.py
@@ -0,0 +1,9 @@
+matriz_3x3 = np.random.randint(0, 2, (3, 3))
+
+# Calculando a soma e a média
+soma = np.sum(matriz_3x3)
+media = np.mean(matriz_3x3)
+
+print("Matriz 3x3:\n", matriz_3x3)
+print("Soma dos elementos:", soma)
+print("Média dos elementos:", media)
